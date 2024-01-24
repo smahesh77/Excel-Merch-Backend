b37e9e38616bc4358da7f7faa11f307ebe15f175
Mayon-Francis <mayonfrancis@gmail.com>
2024-01-25 01:30:53 +0530

    Added logger and mail trnsport for logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import morgan from 'morgan';
 import { userRouter } from './routes/User/UserRoute';
 import { itemRouter } from './routes/ItemRoutes';
 import { PORT, WITH_LOCAL_TUNNEL } from './utils/env';
@@ -8,12 +9,32 @@ import { errorHandler, jsonParseErrHandler } from './middleware/error';
 import { adminRouter } from './routes/Admin/AdminRoutes';
 import localtunnel from 'localtunnel';
 import { webhookRouter } from './routes/Webhook/webhook';
+import { logger } from './utils/logger';
 
 const app = express();
 app.use(express.json());
 app.use(jsonParseErrHandler);
 
 app.use(cors());
+
+app.use(
+	morgan(
+		function (tokens, req, res) {
+			return [
+				tokens.method(req, res),
+				tokens.url(req, res),
+				tokens.status(req, res),
+				tokens['response-time'](req, res),
+				'ms',
+			].join(' ');
+		},
+		{
+			stream: {
+				write: (message) => logger.info(message.trim()),
+			},
+		}
+	)
+);
 
 app.get('/ping', (req: express.Request, res: express.Response) => {
 	res.status(200).json({ message: 'pong', status: 'OK' });
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -51,16 +51,57 @@ if (!process.env.RAZORPAY_TRANSFER_ACC_ID) {
 
 export const RAZORPAY_TRANSFER_ACC_ID = process.env.RAZORPAY_TRANSFER_ACC_ID;
 
-if(!process.env.RAZORPAY_WEBHOOK_SECRET) {
+if (!process.env.RAZORPAY_WEBHOOK_SECRET) {
 	console.error('fatal: RAZORPAY_WEBHOOK_SECRET not set in env');
 	process.exit(1);
 }
 
 export const RAZORPAY_WEBHOOK_SECRET = process.env.RAZORPAY_WEBHOOK_SECRET;
 
-if(!process.env.WITH_LOCAL_TUNNEL || process.env.WITH_LOCAL_TUNNEL !== 'true') {
-	console.log('WITH_LOCAL_TUNNEL not set in env, defaulting to false');
+if (
+	!process.env.WITH_LOCAL_TUNNEL ||
+	process.env.WITH_LOCAL_TUNNEL !== 'true'
+) {
+	console.log(`WITH_LOCAL_TUNNEL not set to 'true', defaulting to false`);
 	console.log('This Means that the webhook will not work');
 }
 
 export const WITH_LOCAL_TUNNEL = process.env.WITH_LOCAL_TUNNEL === 'true';
+
+if (!process.env.NODE_ENV) {
+	console.log('NODE_ENV not set in env, defaulting to production');
+}
+
+export const NODE_ENV = process.env.NODE_ENV || 'production';
+
+if (
+	!process.env.SMTP_USER ||
+	!process.env.SMTP_PASS ||
+	!process.env.SMTP_HOST
+) {
+	console.warn(
+		'One or more of SMTP_USER, SMTP_PASS, SMTP_HOST not set in env, email functionality will not work'
+	);
+}
+if (!process.env.SMTP_PORT) {
+	console.log('SMTP_PORT not set in env, defaulting to 465');
+	console.log(
+		'If you get an error, try setting it to 587 or refer email server docs'
+	);
+}
+if (!process.env.SMTP_FROM) {
+	console.log(
+		'SMTP_FROM not set in env, defaulting to "Excel Merch <noreply@excelmec.org>"'
+	);
+}
+
+if (!process.env.SMTP_TO) {
+	console.log('SMTP_TO not set in env, defaulting to cstech@excelmec.org');
+}
+export const SMTP_USER = process.env.SMTP_USER;
+export const SMTP_PASS = process.env.SMTP_PASS;
+export const SMTP_HOST = process.env.SMTP_HOST;
+export const SMTP_FROM =
+	process.env.SMTP_FROM || 'Excel Merch <noreply@excelmec.org>';
+export const SMTP_TO = process.env.SMTP_TO || 'cstech@excelmec.org'
+export const SMTP_PORT = parseInt(process.env.SMTP_PORT || '465');
