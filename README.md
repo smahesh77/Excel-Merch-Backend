cff7a51563e4960ab01974623fa4d0c0b202532c
Mayon-Francis <mayonfrancis@gmail.com>
2024-01-28 11:33:20 +0530

    Set Cors

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ https://documenter.getpostman.com/view/29362600/2s9YkrZeL2
 
 ### Order Status Flow
 ![orderStatusFlow](assets/Order%20Status.png)
+
+### Some stuff to note
+- The storage bucket must have its cors configured, the backend does this on startup
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,6 @@
 import { Storage } from '@google-cloud/storage';
 import { BUCKET_NAME, GOOGLE_CLOUD_KEY_BASE64 } from './env';
+import { logger } from './logger';
 
 const gcsKeyBuffer = Buffer.from(GOOGLE_CLOUD_KEY_BASE64, 'base64');
 const gcsKey = gcsKeyBuffer.toString('utf-8');
@@ -9,3 +10,30 @@ export const storageClient = new Storage({
 });
 
 export const storageBucket = storageClient.bucket(BUCKET_NAME);
+
+setCorsPolicy();
+
+/**
+ * We set any origin to read the images from the bucket
+ * Setting the origin to the frontend URL would be more secure, if needed
+ * 
+ * After setting the CORS policy, it takes a few minutes to hours to take effect
+ * Depending on what the previous CORS policy maxAgeSeconds was set to
+ */
+async function setCorsPolicy() {
+	try {
+		const corsConfiguration = [
+			{
+				maxAgeSeconds: 5,
+				origin: ['*'],
+				method: ['GET'],
+				responseHeader: ['*'],
+			},
+		];
+		await storageBucket.setCorsConfiguration(corsConfiguration);
+	} catch (err) {
+		logger.error('Error setting CORS policy', {
+			err: JSON.stringify(err),
+		});
+	}
+}
